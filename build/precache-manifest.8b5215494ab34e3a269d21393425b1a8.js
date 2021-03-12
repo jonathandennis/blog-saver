9732c3acff8e0b069ad71023320df099a2c4a310
jonathandennis <4jondennis@gmail.com>
2021-03-12 14:48:43 +0100

    remove firebase and remove build folder from gitignore

---
+++ build/precache-manifest.8b5215494ab34e3a269d21393425b1a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cf6ed968ced60bff927",
+    "url": "/static/css/main.07e3d075.chunk.css"
+  },
+  {
+    "revision": "1cf6ed968ced60bff927",
+    "url": "/static/js/main.31866722.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b8bcb696e9d086853c05",
+    "url": "/static/js/2.e707082f.chunk.js"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "7582e823ef0d702969ea0cce9afb326d",
+    "url": "/static/media/Lato-Italic.7582e823.ttf"
+  },
+  {
+    "revision": "85d339d916479f729938d2911b85bf1f",
+    "url": "/static/media/Lato-Bold.85d339d9.ttf"
+  },
+  {
+    "revision": "8036f27e7114df445f1d21571a49cfd5",
+    "url": "/index.html"
+  }
+];
